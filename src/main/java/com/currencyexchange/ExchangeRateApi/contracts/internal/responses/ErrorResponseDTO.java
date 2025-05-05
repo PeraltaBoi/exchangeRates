b94e97bc33ada020f3546a930aa88e58f0c06b78
PeraltaBoi <tiagofilipeperalta@gmail.com>
2025-05-05 01:04:39 +0100

    rename DTO

--- src/main/java/com/currencyexchange/ExchangeRateApi/contracts/internal/responses/ErrorResponseDTO.java
+++ src/main/java/com/currencyexchange/ExchangeRateApi/contracts/internal/responses/ErrorResponseDTO.java
@@ -3,6 +3,6 @@ package com.currencyexchange.ExchangeRateApi.contracts.internal.responses;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Error response")
-public record ErrorResponse(
+public record ErrorResponseDTO(
 		@Schema(description = "Error message", example = "No exchange rates found for USD") String message) {
 }

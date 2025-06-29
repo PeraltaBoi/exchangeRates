1f972d83d9f06ad7b5451e9d1d82af7cde7a9f74
PeraltaBoi <tiagofilipeperalta@gmail.com>
2025-06-29 18:44:41 +0100

    improve exception handling

--- src/main/java/com/currencyexchange/ExchangeRateApi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/currencyexchange/ExchangeRateApi/exceptions/GlobalExceptionHandler.java
@@ -1,35 +1,29 @@
 package com.currencyexchange.ExchangeRateApi.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.currencyexchange.ExchangeRateApi.contracts.internal.rest.responses.ErrorResponseDTO;
+import com.currencyexchange.ExchangeRateApi.exceptions.interfaces.ICustomStatusException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(ExchangeRateNotFoundException.class)
-	public ResponseEntity<ErrorResponseDTO> handleExchangeRateNotFoundException(ExchangeRateNotFoundException ex) {
-		ErrorResponseDTO error = new ErrorResponseDTO(ex.getMessage());
-		return new ResponseEntity<>(error, ex.getStatusCode());
-	}
+	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-	@ExceptionHandler(ApiKeyNotFoundException.class)
-	public ResponseEntity<ErrorResponseDTO> handleApiKeyNotFoundException(ApiKeyNotFoundException ex) {
-		ErrorResponseDTO error = new ErrorResponseDTO(ex.getMessage());
-		return new ResponseEntity<>(error, ex.getStatusCode());
-	}
+	@ExceptionHandler({
+			UsernameAlreadyExistsException.class,
+			ExchangeRateNotFoundException.class,
+			UnauthorizedException.class,
+			ApiKeyNotFoundException.class
+	})
+	public ResponseEntity<ErrorResponseDTO> handleCustomExceptions(ICustomStatusException ex) {
+		logger.warn("A custom exception was handled: {}", ex.getMessage());
 
-	@ExceptionHandler(UnauthorizedException.class)
-	public ResponseEntity<ErrorResponseDTO> handleUnauthorizedException(UnauthorizedException ex) {
-		ErrorResponseDTO error = new ErrorResponseDTO(ex.getMessage());
-		return new ResponseEntity<>(error, ex.getStatusCode());
-	}
-
-	@ExceptionHandler(UsernameAlreadyExistsException.class)
-	public ResponseEntity<ErrorResponseDTO> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException ex) {
 		ErrorResponseDTO error = new ErrorResponseDTO(ex.getMessage());
 		return new ResponseEntity<>(error, ex.getStatusCode());
 	}
